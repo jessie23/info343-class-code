000a7ad893e225ee94dbb9fc354be6d34dfb683a
Jessie Peterson <jessie23@uw.edu>
2015-10-15 17:12:20 -0700

    Class map code from 10/15

--- jquery/js/app.js
+++ jquery/js/app.js
@@ -5,4 +5,23 @@
     open weather API: 
     http://api.openweathermap.org/data/2.5/weather?zip=98195,us&units=imperial&appid=bd82977b86bf27fb59a04b61b657fb6f
 */
+//When the DOM content has been loaded...
+$(function() {
+	'use strict';
+	//opens a brand new tab when link is clicked for every tag that is 'target'
+	$('a').attr('target', '_blank');
+	$('article').hide().fadeIn(1000);
+	$('#toggle-article').click(function() {
+		$('article').fadeToggle();
+	});
 
+	var url = 'http://api.openweathermap.org/data/2.5/weather?zip=98195,us&units=imperial&appid=bd82977b86bf27fb59a04b61b657fb6f';
+	$.getJSON(url).then(function(data) {
+		console.log(data);
+		var temperature = data.main.temp;
+		$('#temp').text(Math.round(temperature));
+	});
+
+	console.log('test');
+
+});

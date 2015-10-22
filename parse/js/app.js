22c20e8e0f1aa5ffdf83736da29378c3ac2e4e83
Jessie Peterson <jessie23@uw.edu>
2015-10-22 16:52:14 -0700

    Rating code from class 10/22

--- parse/js/app.js
+++ parse/js/app.js
@@ -12,9 +12,13 @@ $(function() {
     //new query that will return all tasks ordered by createAt
     var tasksQuery = new Parse.Query(Task);
     tasksQuery.ascending('createdAt');
+    tasksQuery.notEqualTo('done', true);
 
     //reference to the task list element
     var tasksList = $('#tasks-list');
+
+    //reference to our rating element
+    var ratingElem = $('#rating');
 
     //reference to the error message alert
     var errorMessage = $('#error-message');
@@ -36,7 +40,7 @@ $(function() {
     }
 
     function hideSpinner() {
-        $('fa-spin').hide();
+        $('.fa-spin').hide();
     }
 
     function fetchTasks() {
@@ -54,10 +58,26 @@ $(function() {
     function renderTasks() {
         tasksList.empty();
         tasks.forEach(function(task) {
-            $(document.createElement('li'))
+            var li = $(document.createElement('li'))
                 .text(task.get('title'))
-                .appendTo(tasksList);
+                .addClass(task.get('done') ? 'completed-task' : '')
+                .appendTo(tasksList)
+                .click(function() {
+                    task.set('done', !task.get('done'));
+                    task.save().then(renderTasks, displayError);
+                });
+
+            $(document.createElement('span'))
+                .raty({readOnly: true,
+                    score: (task.get('rating') || 0),
+                    hints: ['crap', 'awful', 'ok', 'nice', 'awesome']})
+                .appendTo(li);
         });
+    }
+
+    function showMessage(message) {
+        message = message || 'Hello';
+        alert(message);
     }
 
     //when the user submits the new task form...
@@ -68,8 +88,10 @@ $(function() {
         var title = titleInput.val();
         var task = new Task();
         task.set('title', title);
+        task.set('rating', ratingElem.raty('score'));
         task.save().then(fetchTasks, displayError).then(function() {
             titleInput.val('');
+            ratingElem.raty('set', {});
         });
 
         return false;
@@ -77,6 +99,9 @@ $(function() {
 
     //go and fetch tasks from Parse
     fetchTasks();
+    hideSpinner();
+
+    ratingElem.raty();
 
     window.setInterval(fetchTasks, 3000);
 });

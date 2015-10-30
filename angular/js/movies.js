effdc4a7bd8eeb5ca089769c51346e756b3dc1fe
Jessie Peterson <jessie23@uw.edu>
2015-10-29 17:21:35 -0700

    angular sorting code for movies 10/29

--- angular/js/movies.js
+++ angular/js/movies.js
@@ -1,9 +1,34 @@
 
-angular.module('Movies', [])
+angular.module('Movies', ['ui.router'])
     .controller('MoviesController', function($scope, $http) {
+        var ratingsMap = {
+            'Not Rated': 0,
+            'G': 1,
+            'PG': 2,
+            'PG-13': 3,
+            'R': 4,
+            'NC-17': 5,
+            'X': 6
+        }
+
+    //get our movies JSON file
         $http.get('data/movies-2014.min.json')
             .then(function(results) {
                 //array of movies
-                $scope.movies = results.data;
+                $scope.movies = results.data.map(function(movie) {
+                    movie.ratingOriginal = ratingsMap[movie.rating];
+                    return movie;
+                });
+
+                $scope.distributors = _.uniq(_.pluck($scope.movies, 'distributor'));
             });
-    });
+
+        $scope.setSort = function(propertyName) {
+            if ($scope.sortCol === propertyName) {
+                $scope.sortReverse = !$scope.sortReverse;
+            } else {
+                $scope.sortCol = propertyName;
+                $scope.sortReverse = false;
+            }
+        }
+    })
